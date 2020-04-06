f688bde20442fcc6a8b1f1b8c7acd1a88b628df9
ismael <ismaelnos5@gmail.com>
2020-04-06 08:55:40 -0300

    adjust connection db

--- src/main/java/com/project/service/HikariCPDataSource.java
+++ src/main/java/com/project/service/HikariCPDataSource.java
@@ -26,8 +26,17 @@ public class HikariCPDataSource {
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.addDataSourceProperty("maxPoolSize", 20);
+
+        config.setLeakDetectionThreshold(30000);
+        config.setConnectionTimeout(10000);
+        config.setIdleTimeout(60000);
+        config.setMaxLifetime(60000);
 
         ds = new HikariDataSource(config);
+
+
+
         System.out.println("Configuration Database success");
 
     }
