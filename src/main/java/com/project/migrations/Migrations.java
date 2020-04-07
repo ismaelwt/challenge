1b2ba2ddc8ca7916372163db806e207c82615547
ismael <ismaelnos5@gmail.com>
2020-04-06 21:16:06 -0300

    code

--- src/main/java/com/project/migrations/Migrations.java
+++ src/main/java/com/project/migrations/Migrations.java
@@ -3,23 +3,24 @@ package com.project.migrations;
 import com.project.service.HikariCPDataSource;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Migrations {
 
 
 
-    private static final String SQL_CREATE_TABLE = "DROP TABLE IF EXISTS person; CREATE TABLE person (id character varying NOT null, name character varying(50), last_name character varying(50), age integer NOT NULL, CONSTRAINT person_pkey PRIMARY KEY (id), CONSTRAINT person_name_key UNIQUE (name));";
+
 
     public static void run() {
 
-        try {
-            System.out.println("Running migrations ... ");
-            Connection conn = HikariCPDataSource.getConnection();
-            PreparedStatement pst = conn.prepareStatement(SQL_CREATE_TABLE);
-            pst.execute();
-        } catch (SQLException e) {
+        try (Connection conn = HikariCPDataSource.getConnection(); Statement stmt = conn.createStatement()) {
+
+
+            String SQL_CREATE_TABLE = "DROP TABLE IF EXISTS person; CREATE TABLE person (id character varying NOT null, name character varying(50), last_name character varying(50), age integer NOT NULL, CONSTRAINT person_pkey PRIMARY KEY (id), CONSTRAINT person_name_key UNIQUE (name));";
+
+            stmt.executeUpdate(SQL_CREATE_TABLE);
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
